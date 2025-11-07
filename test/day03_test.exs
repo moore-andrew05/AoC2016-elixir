7cfc4f9808754787ba7696bf9a1ff46e6bf924a1
Andrew Moore <moore.andrew0598@gmail.com>
2025-11-06 21:03:39 -0700

    Finished day 4, part2 is shite

--- test/day03_test.exs
+++ test/day03_test.exs
@@ -4,13 +4,11 @@ defmodule Aoc2016.Day03Test do
 
   describe "part1/1" do
     test "Codepad part 1" do
-      assert Day03.part1("ULL\nRRDDD\nLURDL\nUUUUD") == [1, 9, 8, 5]
     end
   end
 
   describe "part2/1" do
     test "returns distance of first revisited location" do
-      assert Day03.part2("ULL\nRRDDD\nLURDL\nUUUUD") == ["5", "D", "B", "3"]
     end
   end
 end
