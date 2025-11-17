34493f5f375aaa0c9f38d537a1229ceb2df1000b
Andrew Moore <moore.andrew0598@gmail.com>
2025-11-16 23:14:07 -0700

    Pretty close to something workable

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,3 +1,11 @@
+defmodule RC do
+  def comb(0, _), do: [[]]
+  def comb(_, []), do: []
+  def comb(m, [h|t]) do
+    (for l <- comb(m-1, t), do: [h|l]) ++ comb(m, t)
+  end
+end
+
 defmodule Item do
   @enforce_keys [:element, :type]
   defstruct [
@@ -22,17 +30,23 @@ defmodule FloorState do
 end
 
 defmodule Aoc2016.Day11 do
+  use Agent
+
   defp input do
     String.trim(File.read!("priv/inputs/day11.txt"))
   end
 
+  def start_memo do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
   defp parse_input_row(row) do
     generators =
-      Regex.scan(~r/(?<generators>\w+) generator/, row, capture: :first)
+      Regex.scan(~r/(?<generators>\w+) generator/, row, capture: :all_but_first)
       |> List.flatten()
 
     chips =
-      Regex.scan(~r/(?<microchips>\w+)-compatible microchip/, row, capture: :first)
+      Regex.scan(~r/(?<microchips>\w+)-compatible microchip/, row, capture: :all_but_first)
       |> List.flatten()
 
     generators =
@@ -56,6 +70,7 @@ defmodule Aoc2016.Day11 do
 
         [item | chips]
       end)
+
     %Floor{
       items: generators ++ chips
     }
@@ -68,10 +83,28 @@ defmodule Aoc2016.Day11 do
       |> Enum.map(&parse_input_row(&1))
   end
 
+  defp validate_floor(floor) do
+    has_generator = Enum.any?(floor.items, fn x -> x.type == :gen end)
+
+    elems =
+      floor.items
+      |> Enum.group_by(&{&1.element})
+      |> Map.filter(fn {_key, val} -> length(val) == 1 end)
+      |> Map.filter(fn {_key, val} -> List.first(val).type == :chip end)
+
+    if Map.size(elems) > 0 and has_generator do
+      false
+    else
+      true
+    end
+  end
+
   def part1(), do: part1(input())
 
   def part1(input) do
-    process_input(input)
+    data = process_input(input)
+
+    data
   end
 
   def part2(), do: part2(input())
